71935bd876b3487bbb163dab4586c0f9d3893ba6
bugsounet <bugsounet@bugsounet.fr>
2022-07-24 17:01:21 +0200

    add Greek translation

--- EXT-GooglePhotos.js
+++ EXT-GooglePhotos.js
@@ -53,7 +53,8 @@ Module.register("EXT-GooglePhotos", {
       es: "translations/es.json",
       nl: "translations/nl.json",
       pt: "translations/pt.json",
-      ko: "translations/ko.json"
+      ko: "translations/ko.json",
+      el: "translations/el.json"
     }
   },
 
